d543f322a210781966588df9fcfdf7fcd8cd2b71
naman0058 <jnaman345@gmail.com>
2021-09-22 17:05:24 +0530

    a

---
+++ src/Home.js
@@ -0,0 +1,369 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+
+
+    <header id="header">
+
+<div id="topbar">
+  <div class="container">
+    <div class="social-links">
+      <a href="https://twitter.com/FileMakr" class="twitter"><i class="fa fa-twitter"></i></a>
+      <a href="https://www.facebook.com/FileMakr" class="facebook"><i class="fa fa-facebook"></i></a>
+      <a href="https://www.instagram.com/filemakr" class="instagram"><i class="fa fa-instagram"></i></a>
+      <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+
+  <div class="logo float-left">
+
+    <h1 class="text-light"><a href="/" class="scrollto"><span>FileMakr</span></a></h1>
+
+  </div>
+
+  <nav class="main-nav float-right d-none d-lg-block">
+    <ul>
+      <li class="active"><a href="/">Home</a></li>
+       <li class="drop-down"><a href="">Project Report</a>
+        <ul>
+          <li><a href="/btech-final-year-project-report">B.TECH</a></li>
+          <li><a href="/mtech-final-year-project-report">M.TECH</a></li>
+          <li><a href="/be-final-year-project-report">BE</a></li>
+          <li><a href="/me-final-year-project-report">ME</a></li>
+           <li><a href="/bca-final-year-project-report">BCA</a></li>
+            <li><a href="/mca-final-year-project-report">MCA</a></li>
+        </ul>
+      </li>
+   
+    </ul>
+  </nav>
+
+</div>
+</header>
+
+
+<section id="intro" class="clearfix">
+<div class="container d-flex h-100">
+  <div class="row justify-content-center align-self-center">
+    <div class="col-md-6 intro-info order-md-first order-last">
+      <h2>Build <span>Major & Minor</span><br/>Project File</h2>
+      <p>The #1 Online Major & Minor Project file Generator! we've helped students owners create easy-to-read, highly effective, custom project files.</p>
+      <div>
+     <a href="#services" class="btn-get-started scrollto">Get Started</a>
+      </div>
+    </div>
+
+    <div class="col-md-6 intro-img order-md-last order-first">
+      <img src="/images/hero4.png" alt="filemakr-hero-header-image" class="img-fluid"/>
+    </div>
+  </div>
+
+</div>
+</section>
+
+<main id="main">
+
+<section class="text-center pos-r">
+<div class="container">
+<div class="row">
+  <div class="col-lg-6 col-md-10 ml-auto mr-auto">
+    <div class="section-title">
+      <h2 class="title">Three Simple Step To Get Report</h2>
+    </div>
+  </div>
+</div>
+<div class="row">
+
+  <div class="col-lg-4 col-md-12">
+    <div class="work-process">
+      <div class="box-loader"> <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div class="step-num-box">
+        <div class="step-icon"><span><i class="fa fa-hand-o-up"></i></span>
+        </div>
+        <div class="step-num">01</div>
+      </div>
+      <div class="step-desc">
+        <h4>Choose Project</h4>
+        <p class="mb-0">Choose Project Report as per your choice</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4 col-md-12 md-mt-5">
+    <div class="work-process">
+      <div class="box-loader"> <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div class="step-num-box">
+        <div class="step-icon"><span><i class="fa fa-edit"></i></span>
+        </div>
+        <div class="step-num">02</div>
+      </div>
+      <div class="step-desc">
+        <h4>Edit Report</h4>
+        <p class="mb-0">Edit Report Content </p>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4 col-md-12 md-mt-5">
+    <div class="work-process">
+      <div class="step-num-box">
+        <div class="step-icon"><span><i class="fa fa-download"></i></span>
+        </div>
+        <div class="step-num">03</div>
+      </div>
+      <div class="step-desc">
+        <h4>Dowanload</h4>
+        <p class="mb-0">Download Project Report easily in free of cost.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</section>
+
+<section id="services" class="section-bg">
+  <div class="container">
+
+ <div class="row">
+            <div class="col-md-12 text-center heading-main">
+                <h2 class="heading">B.Tech Trending Project Report</h2>
+                <div class="separator"><i class="fa fa-home below-line-about-icon"></i></div>
+
+            </div>
+        </div>  
+
+    <div class="row">
+
+     
+      <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s">
+        <div class="box">
+          <h4 class="title"><a href="/btech-final-year-project-report">View All</a></h4>
+       
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+</section>
+
+
+
+
+<section class="amazing_feature">
+  <div class="container">
+
+        <div class="row">
+            <div class="col-md-12 text-center heading-main">
+                <h2 class="heading">GET PROJECT REPORT WITH</h2>
+                <div class="separator"><i class="fa fa-handshake-o below-line-about-icon"></i></div>
+
+            </div>
+        </div>      
+    <div class="row">         
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single_feature">
+          <div class="feature_icon"><i class="fa fa-handshake-o"></i></div>
+          <h3>IEEE Standard</h3>
+          <p>Don’t wait for days to have project report completed. </p>  
+        </div>
+      </div>             
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single_feature">
+          <div class="feature_icon"><i class="fa fa-snowflake-o"></i></div>
+          <h3>ER Diagram</h3>
+          <p>It cant get much easier to have quality project report download.</p>    
+        </div>
+      </div>       
+                
+       
+         
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single_feature">
+          <div class="feature_icon"><i class="fa fa-sitemap"></i></div>
+          <h3>DFD Diagram</h3>
+          <p>Get Project Report with three level DFD Diagram.</p>  
+        </div>
+      </div>         
+    </div>     
+
+
+<div class="row">         
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single_feature">
+          <div class="feature_icon"><i class="fa fa-pie-chart"></i></div>
+          <h3>Giant Chart</h3>
+          <p>Don’t wait for days to have project report completed. </p>  
+        </div>
+      </div>             
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single_feature">
+          <div class="feature_icon"><i class="fa fa-tasks"></i></div>
+          <h3>Screenshots</h3>
+          <p>It cant get much easier to have quality project report download.</p>    
+        </div>
+      </div>       
+                
+       
+         
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="single_feature">
+          <div class="feature_icon"><i class="fa fa-info"></i></div>
+          <h3>Indexing</h3>
+          <p>Get Project Report with quality assured.</p>  
+        </div>
+      </div>         
+    </div>  
+
+  </div> 
+</section>
+
+
+
+
+
+
+
+
+
+
+<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+
+
+
+</main>
+
+
+
+
+<footer id="footer" class="section-bg">
+      <div class="footer-top">
+        <div class="container">
+  
+          <div class="row">
+  
+            <div class="col-lg-6">
+  
+              <div class="row">
+  
+                  <div class="col-sm-6">
+  
+                    <div class="footer-info">
+                      <h3>FileMakr</h3>
+                    <b>
+                        <p>The #1 Online Major & Minor Project file Generator! we've helped students owners create easy-to-read, highly effective, custom project files.
+                        </p>
+                        </b>
+                    </div>
+  
+                   
+                  </div>
+  
+                  <div class="col-sm-6">
+                    <div class="footer-links">
+                      <h4>Useful Links</h4>
+                      <ul>
+                        
+                        <li><a href="/terms-and-conditions">Terms and Conditions</a></li>
+                        <li><a href="/privacy-policy">Privacy Policy</a></li>
+                        <li><a href="/aboutus">About Us</a></li>
+                      </ul>
+                    </div>
+  
+                    <div class="footer-links">
+                      <h4>Contact Us</h4>
+                      <p>
+                        <strong>Phone:</strong>  <a href="tel:8319339945" >+91 8319339945</a>
+                        <strong>Email:</strong>  <a href="mailto:filemakr@gmail.com" >filemakr@gmail.com</a>
+                      </p>
+                    </div>
+  
+                    <div class="social-links">
+                      <a href="https://twitter.com/FileMakr" class="twitter"><i class="fa fa-twitter"></i></a>
+                      <a href="https://www.facebook.com/FileMakr" class="facebook"><i class="fa fa-facebook"></i></a>
+                      <a href="https://www.instagram.com/filemakr" class="instagram"><i class="fa fa-instagram"></i></a>
+                      <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                    </div>
+  
+                  </div>
+  
+              </div>
+  
+            </div>
+  
+            <div class="col-lg-6">
+  
+              <div class="form">
+                
+                <h4>Send us a message</h4>
+                <p></p>
+                <form action="/contactus" method="get"  id="contact" class="contactForm">
+                  <div class="form-group">
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                    <div class="validation"></div>
+                  </div>
+                  <div class="form-group">
+                    <input type="number" class="form-control" name="email" id="email" placeholder="Your Mobile Number" data-rule="minlen:10" data-msg="Please enter a valid mobile number" />
+                    <div class="validation"></div>
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                    <div class="validation"></div>
+                  </div>
+                  <div class="form-group">
+                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                    <div class="validation"></div>
+                  </div>
+  
+                  <div id="sendmessage">Your message has been sent. Thank you! Our Team will contact you as soon as possible.</div>
+                  <div id="errormessage">Not Send</div>
+  
+                  <div class="text-center"><button type="submit" title="Send Message">Send Message</button></div>
+                </form>
+              </div>
+  
+            </div>
+  
+            
+  
+          </div>
+  
+        </div>
+      </div>
+  
+      <div class="container">
+        <div class="copyright">
+          &copy; 2019 Copyright <strong>FileMakr</strong>. 
+        </div>
+    <div class="credits">
+        
+            All the links in the footer should remain intact.
+            You can delete the links only if you purchased the pro version.
+            Licensing information: https://bootstrapmade.com/license/
+            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Rapid
+        
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </div>
+    </footer>
+
+
+
+    </div>
+  );
+}
+
+export default App;
